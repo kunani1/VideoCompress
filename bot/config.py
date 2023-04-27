36a826c2402230ef259721a5c5a4be1c9f0261e6
KSBOTSOFFICAL <108800837+KSBOTSOFFICAL@users.noreply.github.com>
2023-04-27 12:16:09 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,23 +6,23 @@ class Config(object):
     # You can keep this default
     SESSION_NAME = "AH_HVEC_CompressBot"
     # Put MongoDB URL
-    DATABASE_URL = "mongodb+srv://hi:hi@cluster0.ux5qu.mongodb.net/cluster0?retryWrites=true&w=majority"
+    DATABASE_URL = "mongodb+srv://ksai:ksai@cluster0.kayzwoi.mongodb.net/cluster0?retryWrites=true&w=majority"
     # get a token from @BotFather
-    TG_BOT_TOKEN = "1916501879:AAFPKQ36ifPfhSWq4VJ24boQm5u0p-u5RJ0"
+    TG_BOT_TOKEN = "5459584173:AAH5gFxLV-hQkspxROAvnh9eYdBNXT10MKY"
     # The Telegram API things
-    APP_ID = "2929027"
-    API_HASH = "2beecc3ee357e6e3f2b2e783d4159f9f"
-    LOG_CHANNEL = "-1001507893914"
-    UPDATES_CHANNEL = "kscompressorbotlogs"
+    APP_ID = "4483436"
+    API_HASH = "8702509bba70a3637777f76083b2c440"
+    LOG_CHANNEL = "-1001505809090"
+    UPDATES_CHANNEL = "COMPRESSORBOTLOGS1"
      # Without `@` LOL
      # Get these values from my.telegram.org
     # array to store the channel ID who are authorized to use the bot
-    AUTH_USERS = "959184369 , 1319693201"
+    AUTH_USERS = "1319693201"
            
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "/app/downloads"
     # Telegram maximum file upload size
-    BOT_USERNAME = "Sushanth_Compressor_bot"
+    BOT_USERNAME = "kscompressorbot"
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 2097152000
